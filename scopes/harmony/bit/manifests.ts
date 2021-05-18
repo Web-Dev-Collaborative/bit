d463e41ca7536d25891a783077a7c9e6082cdc57
Olivier Combe <olivier.combe@gmail.com>
2021-05-18 12:30:24 +0200

    add NgPackagr aspect & support for `bit compile` & `bit build` (#4244)
    - Add a new ngPackagr aspect to compile Angular code
    - Support for build & compile commands (except the preview part in the build command)
    - Refactor Environment to add more details about the different functions (Linter, Dev, Builder, ...) <-- @ranm8 David told me that you're not really fan of refactoring this, but I think that it helps a lot when writing a new environment... let me know if you prefer another way (like maybe exporting those extended interfaces from their own packages, like `BuilderEnv` from `@teambit/builder`?)

--- scopes/harmony/bit/manifests.ts
+++ scopes/harmony/bit/manifests.ts
@@ -62,6 +62,7 @@ import { ExportAspect } from '@teambit/export';
 import { EjectAspect } from '@teambit/eject';
 import { UserAgentAspect } from '@teambit/user-agent';
 import { AngularAspect } from '@teambit/angular';
+import { NgPackagrAspect } from '@teambit/ng-packagr';
 import { BitAspect } from './bit.aspect';
 
 export const manifestsMap = {
@@ -128,6 +129,7 @@ export const manifestsMap = {
   [ApplicationAspect.id]: ApplicationAspect,
   [EjectAspect.id]: EjectAspect,
   [AngularAspect.id]: AngularAspect,
+  [NgPackagrAspect.id]: NgPackagrAspect,
 };
 
 export function isCoreAspect(id: string) {
